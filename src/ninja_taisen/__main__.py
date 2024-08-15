821a154f281f9fe152a0dc982ac535332cde4168
Luke Chapman <59296898+luke-chapman@users.noreply.github.com>
2024-08-15 07:40:07 +0100

    Improve command line usability (#2)

--- src/ninja_taisen/__main__.py
+++ src/ninja_taisen/__main__.py
@@ -8,7 +8,7 @@ from ninja_taisen.game.game_results import GameResults
 from ninja_taisen.game.game_runner import GameRunner
 from ninja_taisen.objects.card import Team
 from ninja_taisen.strategy.strategy import IStrategy
-from ninja_taisen.strategy.strategy_lookup import lookup_strategy
+from ninja_taisen.strategy.strategy_lookup import STRATEGIES, lookup_strategy
 
 log = getLogger(__name__)
 
@@ -37,11 +37,11 @@ def run(games: int, monkey_strategy: IStrategy, wolf_strategy: IStrategy) -> Non
 def main() -> None:
 
     parser = ArgumentParser()
-    parser.add_argument("-g", "--games", type=int, default=1, dest="games")
-    parser.add_argument("-ms", "--monkey-strategy", type=str, default="random", dest="monkey_strategy")
-    parser.add_argument("-ws", "--wolf-strategy", type=str, default="random", dest="wolf_strategy")
-    parser.add_argument("-v", "--verbose", action="count", default=0, dest="verbosity")
-    parser.add_argument("-p", "--profile", action="store_true", default=False, dest="profile")
+    parser.add_argument("--games", type=int, default=1, help="Number of games to simulate")
+    parser.add_argument("--monkey-strategy", type=str, default="random", choices=STRATEGIES)
+    parser.add_argument("--wolf-strategy", type=str, default="random", choices=STRATEGIES)
+    parser.add_argument("--verbosity", action="count", default=0, help="Verbosity level for logging")
+    parser.add_argument("--profile", action="store_true", help="Profile the code")
     options = parser.parse_args()
 
     monkey_strategy = lookup_strategy(options.monkey_strategy)
