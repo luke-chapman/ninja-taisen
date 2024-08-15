35c1b3db9c2f8c07f9e0fd8582d28c949965096d
Luke Chapman <lrrchapman@gmail.com>
2024-08-15 07:39:39 +0100

    Improve command line usability

--- src/ninja_taisen/strategy/strategy_lookup.py
+++ src/ninja_taisen/strategy/strategy_lookup.py
@@ -1,18 +1,27 @@
 from ninja_taisen.strategy.metric import CountMetric, IMetric, PositionMetric, PositionStrengthMetric, StrengthMetric
 from ninja_taisen.strategy.strategy import IStrategy, MetricStrategy, RandomSpotWinStrategy, RandomStrategy
 
+STRATEGIES = [
+    "random",
+    "random_spot_win",
+    "metric_count",
+    "metric_position",
+    "metric_position_strength",
+    "metric_strength",
+]
+
 
 def lookup_strategy(strategy: str) -> IStrategy:
     if strategy == "random":
         return RandomStrategy()
-    if strategy == "random-spot-win":
+    if strategy == "random_spot_win":
         return RandomSpotWinStrategy()
-    if strategy.startswith("metric:"):
-        _, metric = strategy.split(":")
+    if strategy.startswith("metric_"):
+        _, metric = strategy.split("_")
         metrics: dict[str, IMetric] = {
             "count": CountMetric(),
             "position": PositionMetric(),
-            "position-strength": PositionStrengthMetric(),
+            "position_strength": PositionStrengthMetric(),
             "strength": StrengthMetric(),
         }
         return MetricStrategy(metrics[metric])
