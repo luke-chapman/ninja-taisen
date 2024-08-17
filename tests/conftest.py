1a3a176f32c9b17e9b7a5ae0bda75ec5dbab4215
Luke Chapman <lrrchapman@gmail.com>
2024-08-17 08:40:45 +0100

    Tests ready to go

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,12 @@
 import pytest
+from _pytest.config import Parser
+from _pytest.fixtures import FixtureRequest
 
 
-def pytest_addoption(parser) -> None:
+def pytest_addoption(parser: Parser) -> None:
     parser.addoption("--regen", action="store_true", help="Regenerate expected results in regression tests")
 
 
 @pytest.fixture
-def regen(request):
+def regen(request: FixtureRequest) -> None:
     return request.config.getoption("--regen")
