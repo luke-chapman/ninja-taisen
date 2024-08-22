783f6250330944cf2857722e3fab5ce6c458db42
Luke Chapman <lrrchapman@gmail.com>
2024-08-22 16:53:10 +0100

    Remove timestamps from logging

--- tests/algos/test_card_mover.py
+++ tests/algos/test_card_mover.py
@@ -6,7 +6,7 @@ from ninja_taisen.objects.types import Board, Team
 # This test covers a complex move which caught a few bugs in the original card_mover logic
 # See the debug log output below to help you understand what happens internally
 #
-# 2024-08-22 15:49:34 - DEBUG - card_mover - Starting board
+# DEBUG - card_mover - Starting board
 #                     MS3
 #                 MJ4 MS1
 #     MS2         MP2 MR2
@@ -14,8 +14,8 @@ from ninja_taisen.objects.types import Board, Team
 #                         WS1 WJ4 WR3 WP3
 #                         WP1
 #
-# 2024-08-22 15:49:34 - DEBUG - card_mover - Moving team=monkey, dice_roll=2, pile_index=5, card_index=0
-# 2024-08-22 15:49:34 - DEBUG - card_mover - Board after card move, pre-battles
+# DEBUG - card_mover - Moving team=monkey, dice_roll=2, pile_index=5, card_index=0
+# DEBUG - card_mover - Board after card move, pre-battles
 #                             MS3
 #                 MJ4         MS1
 #     MS2         MP2         MR2
@@ -23,101 +23,101 @@ from ninja_taisen.objects.types import Board, Team
 #                         WS1 WJ4 WR3 WP3
 #                         WP1
 #
-# 2024-08-22 15:49:34 - DEBUG - card_mover - remaining_battles=[7]
-# 2024-08-22 15:49:34 - DEBUG - card_mover - Battle between MS3 and WJ4 in pile 7
-# 2024-08-22 15:49:34 - DEBUG - card_mover - Removing MS3 on top of pile 7
-# 2024-08-22 15:49:34 - DEBUG - card_mover - Battle between MS1 and WJ1 in pile 7
-# 2024-08-22 15:49:34 - DEBUG - card_mover - Draw - both cards retreat, schedule adjacent battles
+# DEBUG - card_mover - remaining_battles=[7]
+# DEBUG - card_mover - Battle between MS3 and WJ4 in pile 7
+# DEBUG - card_mover - Removing MS3 on top of pile 7
+# DEBUG - card_mover - Battle between MS1 and WJ1 in pile 7
+# DEBUG - card_mover - Draw - both cards retreat, schedule adjacent battles
 #                 MJ4         MS1
 #     MS2         MP2         MR2
 # --- --- --- --- --- --- --- --- --- --- ---
 #                         WS1 WJ0 WR3 WP3
 #                         WP1
 #
-# 2024-08-22 15:49:34 - DEBUG - card_mover - Moving team=wolf, dice_roll=-1, pile_index=7, card_index=0
-# 2024-08-22 15:49:34 - DEBUG - card_mover - Board after card move, pre-battles
+# DEBUG - card_mover - Moving team=wolf, dice_roll=-1, pile_index=7, card_index=0
+# DEBUG - card_mover - Board after card move, pre-battles
 #                 MJ4         MS1
 #     MS2         MP2         MR2
 # --- --- --- --- --- --- --- --- --- --- ---
 #                         WS1     WR3 WP3
 #                         WP1     WJ0
 #
-# 2024-08-22 15:49:34 - DEBUG - card_mover - Moving team=monkey, dice_roll=-1, pile_index=7, card_index=1
-# 2024-08-22 15:49:34 - DEBUG - card_mover - Board after card move, pre-battles
+# DEBUG - card_mover - Moving team=monkey, dice_roll=-1, pile_index=7, card_index=1
+# DEBUG - card_mover - Board after card move, pre-battles
 #                 MJ4
 #     MS2         MP2     MS1 MR2
 # --- --- --- --- --- --- --- --- --- --- ---
 #                         WS1     WR3 WP3
 #                         WP1     WJ0
 #
-# 2024-08-22 15:49:34 - DEBUG - card_mover - Battle at pile_index 7 resolved - board
+# DEBUG - card_mover - Battle at pile_index 7 resolved - board
 #                 MJ4
 #     MS2         MP2     MS1 MR2
 # --- --- --- --- --- --- --- --- --- --- ---
 #                         WS1     WR3 WP3
 #                         WP1     WJ0
 #
-# 2024-08-22 15:49:34 - DEBUG - card_mover - remaining_battles=[8, 6]
-# 2024-08-22 15:49:34 - DEBUG - card_mover - Battle between MS1 and WP1 in pile 6
-# 2024-08-22 15:49:34 - DEBUG - card_mover - Removing WP1 on top of pile 6
-# 2024-08-22 15:49:34 - DEBUG - card_mover - Battle between MS1 and WS1 in pile 6
-# 2024-08-22 15:49:34 - DEBUG - card_mover - Draw - both cards retreat, schedule adjacent battles
+# DEBUG - card_mover - remaining_battles=[8, 6]
+# DEBUG - card_mover - Battle between MS1 and WP1 in pile 6
+# DEBUG - card_mover - Removing WP1 on top of pile 6
+# DEBUG - card_mover - Battle between MS1 and WS1 in pile 6
+# DEBUG - card_mover - Draw - both cards retreat, schedule adjacent battles
 #                 MJ4
 #     MS2         MP2     MS1 MR2
 # --- --- --- --- --- --- --- --- --- --- ---
 #                         WS1     WR3 WP3
 #                                 WJ0
 #
-# 2024-08-22 15:49:34 - DEBUG - card_mover - Moving team=wolf, dice_roll=-1, pile_index=6, card_index=0
-# 2024-08-22 15:49:34 - DEBUG - card_mover - Board after card move, pre-battles
+# DEBUG - card_mover - Moving team=wolf, dice_roll=-1, pile_index=6, card_index=0
+# DEBUG - card_mover - Board after card move, pre-battles
 #                 MJ4
 #     MS2         MP2     MS1 MR2
 # --- --- --- --- --- --- --- --- --- --- ---
 #                             WS1 WR3 WP3
 #                                 WJ0
 #
-# 2024-08-22 15:49:34 - DEBUG - card_mover - Moving team=monkey, dice_roll=-1, pile_index=6, card_index=0
-# 2024-08-22 15:49:34 - DEBUG - card_mover - Board after card move, pre-battles
+# DEBUG - card_mover - Moving team=monkey, dice_roll=-1, pile_index=6, card_index=0
+# DEBUG - card_mover - Board after card move, pre-battles
 #                 MJ4
 #     MS2         MP2 MS1     MR2
 # --- --- --- --- --- --- --- --- --- --- ---
 #                             WS1 WR3 WP3
 #                                 WJ0
 #
-# 2024-08-22 15:49:34 - DEBUG - card_mover - Battle at pile_index 6 resolved - board
+# DEBUG - card_mover - Battle at pile_index 6 resolved - board
 #                 MJ4
 #     MS2         MP2 MS1     MR2
 # --- --- --- --- --- --- --- --- --- --- ---
 #                             WS1 WR3 WP3
 #                                 WJ0
 #
-# 2024-08-22 15:49:34 - DEBUG - card_mover - remaining_battles=[8, 7, 5]
-# 2024-08-22 15:49:34 - DEBUG - card_mover - Battle at pile_index 5 resolved - board
+# DEBUG - card_mover - remaining_battles=[8, 7, 5]
+# DEBUG - card_mover - Battle at pile_index 5 resolved - board
 #                 MJ4
 #     MS2         MP2 MS1     MR2
 # --- --- --- --- --- --- --- --- --- --- ---
 #                             WS1 WR3 WP3
 #                                 WJ0
 #
-# 2024-08-22 15:49:34 - DEBUG - card_mover - remaining_battles=[8, 7]
-# 2024-08-22 15:49:34 - DEBUG - card_mover - Battle between MR2 and WS1 in pile 7
-# 2024-08-22 15:49:34 - DEBUG - card_mover - Removing WS1 on top of pile 7
-# 2024-08-22 15:49:34 - DEBUG - card_mover - Battle at pile_index 7 resolved - board
+# DEBUG - card_mover - remaining_battles=[8, 7]
+# DEBUG - card_mover - Battle between MR2 and WS1 in pile 7
+# DEBUG - card_mover - Removing WS1 on top of pile 7
+# DEBUG - card_mover - Battle at pile_index 7 resolved - board
 #                 MJ4
 #     MS2         MP2 MS1     MR2
 # --- --- --- --- --- --- --- --- --- --- ---
 #                                 WR3 WP3
 #                                 WJ0
 #
-# 2024-08-22 15:49:34 - DEBUG - card_mover - remaining_battles=[8]
-# 2024-08-22 15:49:34 - DEBUG - card_mover - Battle at pile_index 8 resolved - board
+# DEBUG - card_mover - remaining_battles=[8]
+# DEBUG - card_mover - Battle at pile_index 8 resolved - board
 #                 MJ4
 #     MS2         MP2 MS1     MR2
 # --- --- --- --- --- --- --- --- --- --- ---
 #                                 WR3 WP3
 #                                 WJ0
 #
-# 2024-08-22 15:49:34 - DEBUG - card_mover - Final board
+# DEBUG - card_mover - Final board
 #                 MJ4
 #     MS2         MP2 MS1     MR2
 # --- --- --- --- --- --- --- --- --- --- ---
