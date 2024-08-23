a3116a1fcee33511c913f59a599e53dd256721be
Luke Chapman <lrrchapman@gmail.com>
2024-08-23 15:15:20 +0100

    Speed up regen test

--- tests/regression/test_simulation.py
+++ tests/regression/test_simulation.py
@@ -1,5 +1,4 @@
 import itertools
-import os
 from pathlib import Path
 
 import polars as pl
@@ -11,8 +10,11 @@ from ninja_taisen.api import make_data_frame
 from ninja_taisen.objects.types import ALL_STRATEGY_NAMES
 
 
-def __run_simulation_regression_test(regen: bool, max_processes: int) -> None:
-    expected = Path(__file__).resolve().parent / "results.csv"
+@pytest.mark.parametrize("max_processes", (1, -1))
+def test_all_strategies(max_processes: int, regen: bool) -> None:
+    if regen and max_processes != -1:
+        # We only regenerate the output on the all-threads variant of this test
+        return
 
     instructions: list[InstructionDto] = []
     for index, (monkey_strategy, wolf_strategy) in enumerate(itertools.product(ALL_STRATEGY_NAMES, ALL_STRATEGY_NAMES)):
@@ -29,17 +31,9 @@ def __run_simulation_regression_test(regen: bool, max_processes: int) -> None:
     assert instructions == recovered_instructions
 
     df_actual = make_data_frame(results).drop(["start_time", "end_time", "process_name"])
+    results_csv = Path(__file__).resolve().parent / "results.csv"
     if regen:
-        df_actual.write_csv(expected)
+        df_actual.write_csv(results_csv)
     else:
-        df_expected = pl.read_csv(expected)
+        df_expected = pl.read_csv(results_csv)
         assert_frame_equal(df_expected, df_actual)
-
-
-def test_single_process(regen: bool) -> None:
-    __run_simulation_regression_test(regen=regen, max_processes=1)
-
-
-@pytest.mark.parametrize("max_processes", range(2, os.cpu_count() or 2))
-def test_multi_process(max_processes: int) -> None:
-    __run_simulation_regression_test(regen=False, max_processes=max_processes)
