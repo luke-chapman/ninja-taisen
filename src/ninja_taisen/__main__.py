ee510f425a2a728ddd32ef61866a40d0e3e66b2d
Luke Chapman <lrrchapman@gmail.com>
2024-08-26 13:42:42 +0100

    API to support POST JSON requests

--- src/ninja_taisen/__main__.py
+++ src/ninja_taisen/__main__.py
@@ -1,44 +1,39 @@
-import datetime
-import logging
 import sys
 from argparse import ArgumentParser
 from logging import getLogger
-from pathlib import Path
 
-from ninja_taisen.api import simulate
-from ninja_taisen.dtos import InstructionDto
+from flask import Flask, request
+
+from ninja_taisen.api import choose_move
+from ninja_taisen.dtos import MoveRequestBody, StrategyName
+from ninja_taisen.objects.safe_random import SafeRandom
 from ninja_taisen.objects.types import ALL_STRATEGY_NAMES
 
 log = getLogger(__name__)
 
 
 def main(override_args: list[str] | None = None) -> int:
-
     parser = ArgumentParser()
-    parser.add_argument("--games", type=int, default=1, help="Number of games to simulate")
-    parser.add_argument("--monkey-strategy", type=str, default="random", choices=ALL_STRATEGY_NAMES)
-    parser.add_argument("--wolf-strategy", type=str, default="random", choices=ALL_STRATEGY_NAMES)
-    parser.add_argument("--verbosity", action="count", default=logging.INFO, help="Verbosity level for logging")
-    parser.add_argument("--profile", action="store_true", help="Profile the code")
-    parser.add_argument("--results-file", type=Path, help="Filename to store results in csv format")
+    parser.add_argument("--strategy", default=StrategyName.metric_strength, choices=ALL_STRATEGY_NAMES)
+    parser.add_argument("--seed", default=0)
     args = parser.parse_args(override_args or sys.argv[1:])
+    strategy = args.strategy
+    random = SafeRandom(args.seed)
 
-    results_file = args.results_file
-    if not results_file:
-        timestamp = datetime.datetime.now(datetime.UTC).strftime("%Y%m%d_%H%M%S")
-        results_file = Path.cwd() / ".ninja-taisen" / f"results_{timestamp}.csv"
+    def handle_choose():
+        if not request.is_json:
+            return {"error": "Request must be JSON"}, 400
+        try:
+            data = request.get_json()
+            request_body = MoveRequestBody.model_validate_json(data)
+            response_body = choose_move(request=request_body, strategy_name=strategy, random=random)
+            return response_body.model_dump(round_trip=True, by_alias=True), 200
+        except Exception as e:
+            return {"error": str(e)}, 400
 
-    instructions = [
-        InstructionDto(id=index, seed=index, monkey_strategy=args.monkey_strategy, wolf_strategy=args.wolf_strategy)
-        for index in range(args.games)
-    ]
-
-    results = simulate(
-        instructions=instructions, csv_results=results_file, verbosity=args.verbosity, profile=args.profile
-    )
-    assert len(results) == len(instructions)
-    log.info(f"Successfully simulated {len(results)} games")
-
+    app = Flask(__name__)
+    app.route("/choose", methods=["POST"])(handle_choose)
+    app.run()
     return 0
 
 
