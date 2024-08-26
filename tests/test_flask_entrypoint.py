ab6c17779e44b4f342cd0b17da682259475718f5
Luke Chapman <lrrchapman@gmail.com>
2024-08-26 19:36:34 +0100

    Add second test

--- tests/test_flask_entrypoint.py
+++ tests/test_flask_entrypoint.py
@@ -1,8 +1,9 @@
 import requests
 
-from ninja_taisen import ChooseRequest
+from ninja_taisen import ChooseRequest, execute_move
 from ninja_taisen.algos.board_builder import make_board
-from ninja_taisen.dtos import ChooseResponse, DiceRollDto, TeamDto
+from ninja_taisen.dtos import ChooseResponse, DiceRollDto, TeamDto, ExecuteRequest, MoveDto, CategoryDto, \
+    ExecuteResponse
 from ninja_taisen.objects.safe_random import SafeRandom
 from tests.conftest import validate_choose_response
 
@@ -18,10 +19,36 @@ def test_choose() -> None:
         dice=DiceRollDto(rock=random.roll_dice(), paper=random.roll_dice(), scissors=random.roll_dice()),
         team=team,
     )
-    request_json = choose_request.model_dump(by_alias=True, round_trip=True)
 
-    response = requests.post(url=URL + "/choose", json=request_json)
+    response = requests.post(
+        url=URL + "/choose",
+        json=choose_request.model_dump(by_alias=True, round_trip=True),
+    )
     assert response.status_code == 200, f"status_code={response.status_code}, text={response.text}"
 
     choose_response = ChooseResponse.model_validate(response.json())
     validate_choose_response(choose_response, team)
+
+
+def test_execute() -> None:
+    random = SafeRandom(0)
+    team = TeamDto.wolf
+
+    execute_request = ExecuteRequest(
+        board=make_board(random=random, shuffle_cards=True).to_dto(),
+        dice=DiceRollDto(rock=random.roll_dice(), paper=random.roll_dice(), scissors=random.roll_dice()),
+        team=team,
+        moves=[
+            MoveDto(dice_category=CategoryDto.rock, card="WR2")
+        ]
+    )
+
+    response = requests.post(
+        url=URL + "/execute",
+        json=execute_request.model_dump(by_alias=True, round_trip=True),
+    )
+    assert response.status_code == 200, f"status_code={response.status_code}, text={response.text}"
+
+    actual_response = ExecuteResponse.model_validate(response.json())
+    expected_response = execute_move(execute_request)
+    assert actual_response == expected_response
