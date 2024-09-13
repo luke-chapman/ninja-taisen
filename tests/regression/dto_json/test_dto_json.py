6b2187cd8a08ecb55da28122ad28b6acb7831eac
Luke Chapman <lrrchapman@gmail.com>
2024-09-13 14:58:45 +0100

    Begin adding more json examples

--- tests/regression/dto_json/test_dto_json.py
+++ tests/regression/dto_json/test_dto_json.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+from ninja_taisen import execute_move
 from ninja_taisen.dtos import (
     BoardDto,
     CategoryDto,
@@ -8,7 +9,7 @@ from ninja_taisen.dtos import (
     DiceRollDto,
     MoveDto,
     Strategy,
-    TeamDto,
+    TeamDto, ExecuteRequest, ExecuteResponse
 )
 from ninja_taisen.objects.cards import WP2, WR3
 from ninja_taisen.objects.types import Board
@@ -38,9 +39,11 @@ def test_for_dto_json_changes(regen: bool) -> None:
     board_dto_2 = board.to_dto()
     assert board_dto_1 == board_dto_2
 
+    dice = DiceRollDto(rock=1, paper=3, scissors=2)
+
     choose_request_1 = ChooseRequest(
         board=board_dto_1,
-        dice=DiceRollDto(rock=1, paper=3, scissors=2),
+        dice=dice,
         team=TeamDto.wolf,
         strategy=Strategy.metric_strength,
     )
@@ -51,15 +54,22 @@ def test_for_dto_json_changes(regen: bool) -> None:
         ]
     )
 
+    execute_request_1 = ExecuteRequest(board=board_dto_1, dice=dice, team=TeamDto.wolf, moves=choose_request_1.moves)
+    execute_response_1 = execute_move(execute_request_1)
+
     json_dir = Path(__file__).resolve().parent
     board_json = json_dir / "board.json"
     choose_request_json = json_dir / "choose_request.json"
     choose_response_json = json_dir / "choose_response.json"
+    execute_request_json = json_dir / "execute_request.json"
+    execute_response_json = json_dir / "execute_response.json"
 
     if regen:
         board_dto_1.to_json_file(board_json, indent=None)
         choose_request_1.to_json_file(choose_request_json, indent=None)
         choose_response_1.to_json_file(choose_response_json, indent=None)
+        execute_request_1.to_json_file(execute_request_json, indent=None)
+        execute_response_1.to_json_file(execute_response_json, indent=None)
     else:
         board_content = board_json.read_text()
         board_dto_3 = BoardDto.model_validate_json(board_content)
@@ -72,3 +82,11 @@ def test_for_dto_json_changes(regen: bool) -> None:
         choose_response_content = choose_response_json.read_text()
         choose_response_2 = ChooseResponse.model_validate_json(choose_response_content)
         assert choose_response_2 == choose_response_1
+
+        execute_request_content = execute_request_json.read_text()
+        execute_request_2 = ExecuteRequest.model_validate_json(execute_request_content)
+        assert execute_request_2 == execute_request_1
+
+        execute_response_content = execute_response_json.read_text()
+        execute_response_2 = ExecuteResponse.model_validate_json(execute_response_content)
+        assert execute_response_2 == execute_response_1
