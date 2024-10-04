ac5d6fa3265aff9c1602be978f9f2d3148e32370
Luke Chapman <59296898+luke-chapman@users.noreply.github.com>
2024-10-04 13:27:29 +0100

    Improve new_pile_index() (#69)

--- ninja_taisen_rust/tests/test_lib.rs
+++ ninja_taisen_rust/tests/test_lib.rs
@@ -2,12 +2,12 @@ use tempfile::tempdir;
 use ninja_taisen_rust::{InstructionDto, simulate};
 
 #[test]
-fn test_simulate() {
+fn test_simulate_one() {
     let temp_dir = tempdir().expect("Failed to create temp dir");
     let instructions = vec![
         InstructionDto{
-            id: 1,
-            seed: 2,
+            id: 0,
+            seed: 0,
             monkey_strategy: String::from("metric_strength"),
             wolf_strategy: String::from("wolf_strength")
         }
@@ -19,4 +19,21 @@ fn test_simulate() {
     assert_eq!(results[0].seed, instructions[0].seed);
     assert_eq!(results[0].monkey_strategy, instructions[0].monkey_strategy);
     assert_eq!(results[0].wolf_strategy, instructions[0].wolf_strategy);
+}
+
+#[test]
+fn test_simulate_many() {
+    let temp_dir = tempdir().expect("Failed to create temp dir");
+    let mut instructions = Vec::new();
+    for i in 0..10 {
+        instructions.push(InstructionDto{
+            id: i,
+            seed: i,
+            monkey_strategy: String::from("metric_strength"),
+            wolf_strategy: String::from("wolf_strength")
+        });
+    }
+
+    let results = simulate(&instructions, temp_dir.path());
+    assert_eq!(results.len(), instructions.len());
 }
