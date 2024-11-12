c94fd447bc7e1514ecdb9f62f1a637c7aaaaf9bc
Luke Chapman <lrrchapman@gmail.com>
2024-11-12 18:07:05 +1100

    Initial use of maturin following example in docs

--- ninja_taisen_rust/src/lib.rs
+++ ninja_taisen_rust/src/lib.rs
@@ -7,6 +7,10 @@ mod move_gatherer;
 mod strategy;
 mod metric;
 
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+use pyo3::prelude::*;
 use std::collections::HashMap;
 use std::fs::{create_dir_all, exists, File};
 use chrono::Utc;
@@ -247,6 +251,48 @@ pub fn execute_move(request: &ExecuteRequest) -> ExecuteResponse {
         );
     }
     ExecuteResponse { board: board.to_dto() }
+}
+
+#[pyfunction]
+fn guess_the_number() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
+
+/// A Python module implemented in Rust. The name of this function must match
+/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
+/// import the module.
+#[pymodule]
+fn ninja_taisen_rust(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(guess_the_number, m)?)?;
+    Ok(())
 }
 
 #[cfg(test)]
